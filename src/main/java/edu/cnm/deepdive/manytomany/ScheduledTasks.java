3d6f3de27cdf99f68099bff7955cf987c7a3b6a7
Matthew Morgan <mmorgan55@cnm.edu>
2018-11-29 09:30:36 -0700

    Added a logger for the scheduler.

--- src/main/java/edu/cnm/deepdive/manytomany/ScheduledTasks.java
+++ src/main/java/edu/cnm/deepdive/manytomany/ScheduledTasks.java
@@ -1,14 +1,19 @@
 package edu.cnm.deepdive.manytomany;
 
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
 
+  private static final Logger LOG = LoggerFactory.getLogger(ScheduledTasks.class);
+
   @Scheduled(fixedRate = 5000)
   public void currentTime() {
+    LOG.info(new Date().toString());
     System.out.println(new Date());
   }
 
